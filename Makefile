fea2294c71544e67aebb539b27e9363e340f3179
clif <clifalison@gmail.com>
2017-05-05 14:40:39 -0500

    run antlr from setup.py

--- Makefile
+++ Makefile
@@ -1,11 +1,5 @@
-ANTLR4=java -jar .antlr/antlr-4.7-complete.jar
-
-.antlr/antlr-4.7-complete.jar:
-	mkdir -p .antlr && cd .antlr && curl -O http://www.antlr.org/download/antlr-4.7-complete.jar
-
-grammar: .antlr/antlr-4.7-complete.jar ren.g4
-	$(ANTLR4) -Dlanguage=Python2 -visitor -no-listener -o ren/py2grammar ren.g4 && touch ren/py2grammar/__init__.py
-	$(ANTLR4) -Dlanguage=Python3 -visitor -no-listener -o ren/py3grammar ren.g4 && touch ren/py3grammar/__init__.py
+grammar:
+	python setup.py grammar
 
 test:
 	pytest --cov=ren --cov-report=html
--- setup.py
+++ setup.py
@@ -1,8 +1,55 @@
-from setuptools import setup
+import os
+import platform
+from setuptools import setup, Command
+from subprocess import check_output
+
+
+class GrammarCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if platform.python_version().startswith('2'):
+            from urllib2 import urlopen
+        else:
+            from urllib.request import urlopen
+
+        ANTLR_URL = 'http://www.antlr.org/download/antlr-4.7-complete.jar'
+        ANTLR_DIR = '.antlr'
+        ANTLR_JAR = os.path.join(ANTLR_DIR, 'antlr-4.7-complete.jar')
+        ANTLR_CMD = ["java", "-jar", ANTLR_JAR]
+
+        if not os.path.exists(ANTLR_JAR):
+            if not os.path.exists(ANTLR_DIR):
+                os.mkdir(ANTLR_DIR)
+            with open(ANTLR_JAR, 'wb') as f:
+                f.write(urlopen(ANTLR_URL).read())
+
+        output = check_output(ANTLR_CMD + [
+            "-Dlanguage=Python2", "-visitor", "-no-listener",
+            "-o", "ren/py2grammar", "ren.g4"])
+        if output:
+            self.announce(output)
+        open('ren/py2grammar/__init__.py', 'a').close()
+
+        output = check_output(ANTLR_CMD + [
+            "-Dlanguage=Python3", "-visitor", "-no-listener",
+            "-o", "ren/py3grammar", "ren.g4"])
+        if output:
+            self.announce(output)
+        open('ren/py3grammar/__init__.py', 'a').close()
 
 
 setup(
     name="ren",
     version="0.0.1",
-    packages=["ren"]
+    packages=["ren"],
+    cmdclass={
+        'grammar': GrammarCommand
+    }
 )
